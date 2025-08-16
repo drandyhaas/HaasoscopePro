12f42ed6a7e6922332b8e953e5558e2ac066bf3b
drandyhaas <drandyhaas@gmail.com>
2025-08-16 12:30:30 -0400

    Mac build for Monterey target (?) Add mac build script

---
+++ software/macpyinstaller.sh
@@ -0,0 +1,9 @@
+export MACOSX_DEPLOYMENT_TARGET=12.0 # Target macOS Monterey
+python3 -m PyInstaller HaasoscopeProQt.py
+cd dist
+rm -rf Mac_HaasoscopeProQt/HaasoscopeProQt
+rm -rf Mac_HaasoscopeProQt/_internal
+mv HaasoscopeProQt/* Mac_HaasoscopeProQt
+rmdir HaasoscopeProQt
+cd ..
+rm -rf build
