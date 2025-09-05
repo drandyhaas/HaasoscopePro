7252396df91cfe8800ae2a43627cb3bd96cbc0bb
Andy <drandyhaas@gmail.com>
2025-09-05 11:29:27 -0400

    windows build 28.03

--- software/HaasoscopeProQt.py
+++ software/HaasoscopeProQt.py
@@ -240,7 +240,7 @@ class MainWindow(TemplateBaseClass):
         QMessageBox.about(
             self,  # Parent widget (optional, but good practice)
             "Haasoscope Pro Qt, by DrAndyHaas",  # Title of the About dialog
-            "A PyQt5 application for the Haasoscope Pro\n\nVersion 28.02"  # Text content
+            "A PyQt5 application for the Haasoscope Pro\n\nVersion 28.03"  # Text content
         )
 
     def recordtofile(self):
