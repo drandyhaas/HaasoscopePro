c96be844d33604db44831e15f08da3a674743e36
Andy <drandyhaas@gmail.com>
2025-09-10 10:27:40 -0400

    firmware v29, do firmware reload after updating

--- software/HaasoscopeProQt.py
+++ software/HaasoscopeProQt.py
@@ -253,7 +253,7 @@ class MainWindow(TemplateBaseClass):
         QMessageBox.about(
             self,  # Parent widget (optional, but good practice)
             "Haasoscope Pro Qt, by DrAndyHaas",  # Title of the About dialog
-            "A PyQt5 application for the Haasoscope Pro\n\nVersion 28.04"  # Text content
+            "A PyQt5 application for the Haasoscope Pro\n\nVersion 29.04"  # Text content
         )
 
     def recordtofile(self):
