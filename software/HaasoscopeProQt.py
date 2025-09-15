3878bdd9934942c9a96cd0a1c96335b19677f3bb
Andy <drandyhaas@gmail.com>
2025-09-15 17:37:16 -0400

    29.07 winbuild

--- software/HaasoscopeProQt.py
+++ software/HaasoscopeProQt.py
@@ -286,7 +286,7 @@ class MainWindow(TemplateBaseClass):
         QMessageBox.about(
             self,  # Parent widget (optional, but good practice)
             "Haasoscope Pro Qt, by DrAndyHaas",  # Title of the About dialog
-            "A PyQt5 application for the Haasoscope Pro\n\nVersion 29.06"  # Text content
+            "A PyQt5 application for the Haasoscope Pro\n\nVersion 29.07"  # Text content
         )
 
     def persist(self):
