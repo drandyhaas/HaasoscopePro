12ed1d55a04f5cb99398aa20a2b9c05f67cb7b78
Andy <drandyhaas@gmail.com>
2025-10-08 11:52:46 -0400

    build scripts for release dir

--- software/macpyinstaller.sh
+++ software/macpyinstaller.sh
@@ -1,10 +1,22 @@
-export MACOSX_DEPLOYMENT_TARGET=12.0 # Target macOS Monterey
-/usr/bin/python3 -m PyInstaller HaasoscopeProQt.py
-cd dist
-rm -rf Mac_HaasoscopeProQt/HaasoscopeProQt
-rm -rf Mac_HaasoscopeProQt/_internal
-mv HaasoscopeProQt/* Mac_HaasoscopeProQt
-rmdir HaasoscopeProQt
-cd ..
 rm -rf build
-cp *.ui icon.png dist/Mac_HaasoscopeProQt/
+rm -rf dist
+
+/usr/bin/python3 -m PyInstaller HaasoscopeProQt.py
+
+mkdir ../../HaasoscopePro_dist
+rm -rf ../../HaasoscopePro_dist/Mac_HaasoscopeProQt
+mkdir ../../HaasoscopePro_dist/Mac_HaasoscopeProQt
+
+rmdir /s /q "../../HaasoscopePro_dist/adc board firmware"
+mkdir "../../HaasoscopePro_dist/adc board firmware"
+mkdir "../../HaasoscopePro_dist/adc board firmware/output_files"
+cp "../adc board firmware/output_files/coincidence_auto.rpd" "../../HaasoscopePro_dist/adc board firmware/output_files/"
+
+mv dist/HaasoscopeProQt/HaasoscopeProQt.exe ../../HaasoscopePro_dist/Mac_HaasoscopeProQt/
+mv dist/HaasoscopeProQt/_internal ../../HaasoscopePro_dist/Mac_HaasoscopeProQt/
+
+cp *.ui ../../HaasoscopePro_dist/Mac_HaasoscopeProQt/
+cp icon.png ../../HaasoscopePro_dist/Mac_HaasoscopeProQt/
+
+rm -rf build
+rm -rf dist
--- software/windowspyinstaller.bat
+++ software/windowspyinstaller.bat
@@ -1,10 +1,22 @@
-python -m PyInstaller HaasoscopeProQt.py
-cd dist
-move HaasoscopeProQt\HaasoscopeProQt.exe Windows_HaasoscopeProQt\
-rmdir /s /q Windows_HaasoscopeProQt\_internal
-move HaasoscopeProQt\_internal Windows_HaasoscopeProQt\
-rmdir /q HaasoscopeProQt
-cd ..
 rmdir /s /q build
-copy *.ui dist\Windows_HaasoscopeProQt\
-copy icon.png dist\Windows_HaasoscopeProQt\
+rmdir /s /q  dist
+
+python -m PyInstaller HaasoscopeProQt.py
+
+mkdir ..\..\HaasoscopePro_dist
+rmdir /s /q ..\..\HaasoscopePro_dist\Windows_HaasoscopeProQt
+mkdir ..\..\HaasoscopePro_dist\Windows_HaasoscopeProQt
+
+rmdir /s /q "..\..\HaasoscopePro_dist\adc board firmware"
+mkdir "..\..\HaasoscopePro_dist\adc board firmware"
+mkdir "..\..\HaasoscopePro_dist\adc board firmware\output_files"
+copy "..\adc board firmware\output_files\coincidence_auto.rpd" "..\..\HaasoscopePro_dist\adc board firmware\output_files\"
+
+move dist\HaasoscopeProQt\HaasoscopeProQt.exe ..\..\HaasoscopePro_dist\Windows_HaasoscopeProQt\
+move dist\HaasoscopeProQt\_internal ..\..\HaasoscopePro_dist\Windows_HaasoscopeProQt\
+
+copy *.ui ..\..\HaasoscopePro_dist\Windows_HaasoscopeProQt\
+copy icon.png ..\..\HaasoscopePro_dist\Windows_HaasoscopeProQt\
+
+rmdir /s /q build
+rmdir /s /q  dist
