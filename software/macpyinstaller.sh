86aeecf688dd69d4adbd1117f5aa4ba4d1947e3a
drandyhaas <drandyhaas@gmail.com>
2025-10-08 12:00:21 -0400

    mac build script

--- software/macpyinstaller.sh
+++ software/macpyinstaller.sh
@@ -3,16 +3,16 @@ rm -rf dist
 
 /usr/bin/python3 -m PyInstaller HaasoscopeProQt.py
 
-mkdir ../../HaasoscopePro_dist
+mkdir -p ../../HaasoscopePro_dist
 rm -rf ../../HaasoscopePro_dist/Mac_HaasoscopeProQt
 mkdir ../../HaasoscopePro_dist/Mac_HaasoscopeProQt
 
-rmdir /s /q "../../HaasoscopePro_dist/adc board firmware"
+rm -rf "../../HaasoscopePro_dist/adc board firmware"
 mkdir "../../HaasoscopePro_dist/adc board firmware"
 mkdir "../../HaasoscopePro_dist/adc board firmware/output_files"
 cp "../adc board firmware/output_files/coincidence_auto.rpd" "../../HaasoscopePro_dist/adc board firmware/output_files/"
 
-mv dist/HaasoscopeProQt/HaasoscopeProQt.exe ../../HaasoscopePro_dist/Mac_HaasoscopeProQt/
+mv dist/HaasoscopeProQt/HaasoscopeProQt ../../HaasoscopePro_dist/Mac_HaasoscopeProQt/
 mv dist/HaasoscopeProQt/_internal ../../HaasoscopePro_dist/Mac_HaasoscopeProQt/
 
 cp *.ui ../../HaasoscopePro_dist/Mac_HaasoscopeProQt/
